803124173c50a899861f1e16cd026182fc700a33
users <jam.altamirano@yavirac.edu.ec>
2024-12-16 07:51:54 -0500

    se habilito el cors para que se pueda hcer la peticion desde el cliente

--- src/main/java/com/example/back/users/controller/UserController.java
+++ src/main/java/com/example/back/users/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.back.users.model.User;
 import org.springframework.http.ResponseEntity;
 import com.example.back.users.dto.UserRequestDto;
 import com.example.back.users.service.UserService;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("api-users")
 public class UserController {
